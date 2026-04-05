d62e1f313288b1dc6adb6324653baf36f8ae7ecb
michael <michael@local>
2026-04-05 20:08:06 +0200

    fix collector and provide html graph

--- collect-stats.sh
+++ collect-stats.sh
@@ -7,13 +7,17 @@ if [ ! -d "$reportsdir" ] ; then
 	exit 1
 fi
 
+categories=$(find "$reportsdir" -name stats.txt -print0 | \
+  xargs -0 grep -h SUCCESS | \
+  sed -e "s/run_//;s/^[^_]*_//;s/_.*.txt://;s/ SUCCESS : /\t/;s/ *s *$//" | \
+  cut -f1 | awk '!seen[$0]++')
+
 showtitle=1
 for f in $(find "$reportsdir" -name stats.txt); do
 	tim=$(cat $f | grep SUCCESS |\
 		sed -e "s/run_//;s/^[^_]*_//;
 			s/_.*.txt://;s/ SUCCESS : /\t/;s/ *s *$//")
-#printf "%s\n" "$tim" >&2
-	categories=$(printf "%s" "$tim" | cut -f1|uniq)
+printf "%s - %s\n" "$f" "$tim" >&2
 	if [ $showtitle == 1 ] ; then
 		echo -n "system"
 		for c in $(echo $categories); do
@@ -25,9 +29,9 @@ for f in $(find "$reportsdir" -name stats.txt); do
 	printf "%-32s" $(echo -n "$f" | sed -e "s#/stats.txt##;s#.*/##")
 	total=0
 	for c in $(echo $categories); do
-		n=$(printf "%s" "$tim"|grep $c|sort -nk2|head -3|\
-			awk '{ sum += $2; n++ } END { if (n > 0) print sum / n; }'|\
-			awk '{print int($1+0.5)}')
+		n=$(printf "%s" "$tim" | awk -v c="$c" '$1 == c { print $2 }' | \
+			sort -n | head -3 | \
+			awk '{ sum += $1; n++ } END { if (n > 0) print int(sum / n + 0.5); else print 0; }')
 		total=$((total+n))
 		printf "\t%s" "$n"
 		#cut -f2|sort -n|head -3 
